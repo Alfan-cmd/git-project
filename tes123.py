e5169b4438db70f5e61e75fd83dd033943784682
Alfan-cmd <alfanaditiansyah@student.upi.edu>
2025-11-26 11:48:33 +0700

    moga conflict

--- tes123.py
+++ tes123.py
@@ -12,7 +12,22 @@ print("this and that")
 #dfgdbqhqer#addaferg4bbedbw
 #dfgdbqhqer
 a = b 
-
+print("this and that")
+#dfgdbqhqer#addaferg4bbedbw
+#dfgdbqhqer# sgehesg
+#addaferg4bbedbw
+#dfgdbqhqer#addaferg4bbedbw
+print("this and that")print("this and that")
+#dfgdbqhqer#addaferg4bbedbw
+#dfgdbqhqer# sgehesg
+#addaferg4bbedbw
+#dfgdbqhqer#addaferg4bbedbw
+print("this and that")print("this and that")
+#dfgdbqhqer#addaferg4bbedbw
+#dfgdbqhqer# sgehesg
+#addaferg4bbedbw
+#dfgdbqhqer#addaferg4bbedbw
+print("this and that")
 # sgehesg
 #addaferg4bbedbw
 #dfgdbqhqer#addaferg4bbedbw
